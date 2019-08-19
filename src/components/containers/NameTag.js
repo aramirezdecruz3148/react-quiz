23a97647a8353f2103340e7f714ad660e7491b5d
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-19 14:34:27 -0700

    finished quiz functionality, hooked everything up to NameTag, then NameTag to App and created a snapshot

--- src/components/containers/NameTag.js
+++ src/components/containers/NameTag.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import TextSelector from '../nametag-pieces/TextSelector';
+import ColorSelector from '../nametag-pieces/ColorSelector';
+import Shape from '../nametag-pieces/Shape';
 
 export default class NameTag extends Component {
-  render() {
-    return (
+  state = {
+    text: '',
+    backgroundColor: '',
+    textColor: '',
+  }
+  onInputChange = ({ target }) => {
+    this.setState({ [target.name]: target.value, [target.name]: target.value, [target.name]: target.value });
+  }
 
+  render() {
+    const { text, backgroundColor, textColor } = this.state;
+    return (
+      <>
+        <TextSelector 
+          text={text} 
+          onTextChange={this.onInputChange} 
+        />
+        <ColorSelector 
+          textColor={textColor} 
+          backgroundColor={backgroundColor} 
+          onBackgroundChange={this.onInputChange}
+          onTextColorChange={this.onInputChange}
+        />
+        <Shape 
+          text={text}
+          backgroundColor={backgroundColor}
+          textColor={textColor}
+        />
+      </>
     );
   }
 }
